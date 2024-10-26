9fd97c9f91ac44f74d9b23149cb5f34a4a75c7e2
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2024-10-26 01:12:40 +0100

    feat: Introduce prefers-reduce-motion media query to the project details swap animation

--- script.js
+++ script.js
@@ -4,27 +4,51 @@ const imageWrappers = document.querySelectorAll(".project-image_wrapper");
 const defaultProjectText = "projects";
 
 const swapProjectDetails = (projectTitleAttribute, projectNumText) => {
-  const swapProjectDetailsTl = gsap.timeline({ defaults: { duration: 1 } });
+  let mm = gsap.matchMedia(),
+    breakPoint = 750;
 
-  swapProjectDetailsTl
-    .to(projectTitle, {
-      y: "150%",
-      rotation: 7,
-      onComplete: () => (projectTitle.textContent = projectTitleAttribute),
-      ease: "power2.in",
-    })
-    .to(
-      projectNumber,
-      {
-        y: "150%",
-        rotation: 7,
-        onComplete: () => (projectNumber.textContent = projectNumText),
-        ease: "power2.in",
-      },
-      "-=0.8"
-    )
-    .to(projectTitle, { y: "0%", ease: "power2.out", rotation: 0 }, 0.95)
-    .to(projectNumber, { y: "0%", ease: "power2.out", rotation: 0 }, "-=0.8");
+  mm.add(
+    {
+      isDesktop: `(min-width: ${breakPoint}px)`,
+      isMobile: `(max-width: ${breakPoint - 1}px)`,
+      reduceMotion: "(prefers-reduced-motion: reduce)",
+    },
+    (context) => {
+      let { isDesktop, isMobile, reduceMotion } = context.conditions;
+
+      const swapProjectDetailsTl = gsap.timeline({
+        defaults: { duration: reduceMotion ? 0 : 1 },
+      });
+
+      swapProjectDetailsTl
+        .to(projectTitle, {
+          y: reduceMotion ? "0%" : "150%",
+          rotation: reduceMotion ? 0 : 7,
+          onComplete: () => (projectTitle.textContent = projectTitleAttribute),
+          ease: "power2.in",
+        })
+        .to(
+          projectNumber,
+          {
+            y: reduceMotion ? "0%" : "150%",
+            rotation: reduceMotion ? 0 : 7,
+            onComplete: () => (projectNumber.textContent = projectNumText),
+            ease: "power2.in",
+          },
+          reduceMotion ? 0 : "-=0.8"
+        )
+        .to(
+          projectTitle,
+          { y: "0%", ease: "power2.out", rotation: 0 },
+          reduceMotion ? 0 : 0.95
+        )
+        .to(
+          projectNumber,
+          { y: "0%", ease: "power2.out", rotation: 0 },
+          reduceMotion ? 0 : "-=0.8"
+        );
+    }
+  );
 };
 
 imageWrappers.forEach((imageWrapper) => {
